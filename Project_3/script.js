4a09e568142b4769382529608ff1810c7a0b8ef5
Dana <dana_kurniawan@brown.edu>
2020-02-05 13:27:45 -0500

    compilation site

---
+++ Project_3/script.js
@@ -0,0 +1,8 @@
+let "img-hover-zoom" =  document.getElementById("pics");
+img-hover-zoom.addEventListener("mousedown",
+
+
+let titleColor = document.getElementById("tC");
+titleColor.addEventListener("mouseover", function() {
+  titleColor.style.color: #F55650;
+});
